55de3e5317d3383685b5d12652dc5da1798da26e
Jakub Cezary Kolando <kuba.kolando.02.01@gmail.com>
2025-03-11 16:08:21 +0100

    Creating api endpoint for login

---
+++ src/app/api/auth/login/route.ts
@@ -0,0 +1,29 @@
+"use server"
+
+import { NextResponse } from "next/server";
+import { supabaseCreateClient } from "@/utils/supabase/client";
+
+export async function POST(req: Request) {
+  try {
+
+    const supabase = supabaseCreateClient();
+    
+    const {email, password} = await req.json();
+    
+    const { data, error } = await supabase.auth.signInWithPassword({
+      email,
+      password,
+    });
+
+    if(error){
+      console.log("Error signing client:", error);
+      return NextResponse.json({ error: error.message }, { status: 400 });
+    }
+
+    console.log("User logged in");
+    return NextResponse.json({session: data.session});
+  } catch (error) {
+    console.error("API error:", error);
+    return NextResponse.json({ error: "Internal Server Error" }, { status: 500 });
+  }
+}
